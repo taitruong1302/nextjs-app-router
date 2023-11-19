d121f100d7d7c35c046a4567926327ea0efd341b
Truong Huu Tai <taitruong1302@gmail.com>
2023-11-19 21:27:02 +0700

    add suspense to post page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -10,7 +10,7 @@ export default async function PostListPage() {
         <div className='flex flex-col items-center border'>
             <div className='w-1/2 flex flex-col gap-2'>
                 <h1 className='text-3xl font-medium'>List Posts</h1>
-                <Suspense>
+                <Suspense fallback={<Loading />}>
                     <PostList data={data} />
                 </Suspense>
             </div>
