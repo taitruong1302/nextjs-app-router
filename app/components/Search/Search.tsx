0ae3179ef3bd92d30467d11e71a6e7db41a2b87c
Truong Huu Tai <taitruong1302@gmail.com>
2023-11-19 21:22:22 +0700

    customize search bar

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -4,7 +4,7 @@ import { Input } from '@nextui-org/react'
 export default function Search({ setSearch }: any) {
     return (
         <div>
-            <Input type='text' label={'Search'} placeholder='What are you looking for?' onChange={e => setSearch(e.target.value)} />
+            <Input variant='bordered' type='text' label={'Search'} placeholder='What are you looking for?' onChange={e => setSearch(e.target.value)} />
         </div>
     )
 }
