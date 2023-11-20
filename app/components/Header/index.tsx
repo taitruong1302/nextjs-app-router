f370df766d7a05b9aa6914123a3759b632e9b2bc
Tai Truong <taitruong1302@gmail.com>
2023-11-20 08:34:15 +0700

    fix browser reload every time navigate to others link

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,14 +1,14 @@
-'use client'
 import React from 'react'
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Header() {
     const pathname = usePathname()
     return (
         <Navbar className='bg-orange-100'>
             <NavbarContent>
-                <NavbarItem>
+                <NavbarItem isActive={pathname === '/'}>
                     <Link href="/">
                         Home
                     </Link>
