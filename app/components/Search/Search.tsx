87e156dc135bcb408c4ff6703917137ce7039eb6
Tai Truong <taitruong1302@gmail.com>
2023-11-20 11:30:58 +0700

    change delay time

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -3,10 +3,12 @@ import { Input } from '@nextui-org/react'
 import _, { set } from 'lodash'
 
 export default function Search({ setSearch }: any) {
+    console.log("Search Rendered");
+
     return (
         <div>
             <Input variant='bordered' type='text' label={'Search'} placeholder='What are you looking for?' onChange={
-                _.debounce(e => setSearch(e.target.value), 300)
+                _.debounce(e => setSearch(e.target.value), 500)
             } />
         </div>
     )
